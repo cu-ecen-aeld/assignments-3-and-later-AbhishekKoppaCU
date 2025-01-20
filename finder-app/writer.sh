7ec5e71426d5f68461248c49cef8d9a9d938a4d0
Abhishek Koppa <abhishek.koppa@colorado.edu>
2025-01-20 10:55:03 -0700

    Update writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check if exactly two arguments are provided
+# only 2 arguments to be provided
 if [ "$#" -ne 2 ]; then
     echo "Error: Two arguments required - <writefile> <writestr>"
     exit 1
@@ -10,14 +10,14 @@ fi
 writefile=$1
 writestr=$2
 
-# Create the directory path if it doesn't exist
+# Create the directory path 
 mkdir -p "$(dirname "$writefile")"
 
-# Write the string to the file, overwriting if it exists
+# Write the string to the file
 if ! echo "$writestr" > "$writefile"; then
     echo "Error: Could not create or write to the file '$writefile'"
     exit 1
 fi
 
-# Successful execution
+# On Successful execution
 exit 0
